45849a0c1467336c52993033e85d54f53abc4360
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2021-12-01 20:01:24 -0500

    Make Movies holder of props for MovieModal. Refactor appropriately.

--- src/App.js
+++ src/App.js
@@ -7,19 +7,11 @@ import './MovieModal.css'
 import MovieModal from './MovieModal'
 
 function App() {
-  //declare new state var to control whether Modal is shown or not. Var named show, useState sets the default value of this state car to "false". setShow is the function that will update show
-  const [show, setShow] = useState(false)
+  
 
   return (
     <div className="App">
-       {/* onClick calls the setShow function to change show's value to true, thus "showing" the modal. */}
-        <button onClick={() => setShow(true) }>
-          Show MovieModal
-        </button>
-        {/* onClose is the function passed to MovieModal. onClick in Modal calls onClose which changes show to false, thus closing the Modal */}
-        <MovieModal onClose={() => setShow(false)} show={show}>
-          <p>This is modal body</p>
-        </MovieModal>
+       <Movies />
     </div>
   );
 }
