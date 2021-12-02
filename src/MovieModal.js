8f1560cbaa957991d4c65f9179a347a5e5cc9837
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2021-12-01 19:16:09 -0500

    Create css styling to add animation styling to the opening and closing of the MovieModal

--- src/MovieModal.js
+++ src/MovieModal.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const MovieModal = props => {
-    if (!props.show) {
-        return null
-    }
+    
+
     return(
-        //onClick here allows clicking outside of the modal to close the modal by calling onClose from App.js
-        <div className='modal' onClick={props.onClose}>
+        //onClick here allows clicking outside of the modal to close the modal by calling onClose from App.js. 
+        //if props.show is true, then we will have an extra className called .show which will animate via css
+        <div className={`modal ${props.show ? 'show' : ''}`} onClick={props.onClose}>
         {/* stopPropagation here stops the closing on click function to be called until it is called again in its specific inner div */}
             <div className='modal-content' onClick={e => e.stopPropagation()}>
                 <div className='modal-header'>
